bed00f823f407a97ddc451a16d02302b7b1fb4b0
wanggq <875810890@qq.com>
2021-02-20 17:17:25 +0800

    爬虫优化

--- blogserver/src/main/java/com/wpy/BlogserverApplication.java
+++ blogserver/src/main/java/com/wpy/BlogserverApplication.java
@@ -3,11 +3,13 @@ package com.wpy;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
 @EnableScheduling//开启定时任务支持
+//@ComponentScan(basePackages = {"com.wpy"})
 @MapperScan(basePackages = "com.wpy.mapper")
 public class BlogserverApplication {
 
